13b38f62ab90b688d971d238d71b109650ec3061
Cameron Lee <cal26@sfu.ca>
2025-11-11 19:34:30 -0800

    enclose main run loop in episode loop and start working on yaml hyperparameters

--- agent.py
+++ agent.py
@@ -1,34 +1,31 @@
 import torch
 import numpy as np
 from connect4_dqn import ConnectFourDQN
-
+import yaml
 
 class ConnectFourAgent():
-  def __init__(self, agent_num):
+  def __init__(self):
     self.dqn = ConnectFourDQN()
-    self.agent_num = agent_num
 
-  def convert_state_to_NN_input(self, c4_state, agent_num):
+  def convert_state_to_NN_input(self, c4_state):
     """
     Convert a (6, 7, 2) array with values 0 and 1 into a (6, 7) array with values -1, 0, and 1.
-    Depending on the agent calling this function, converts the opposing agent's 1 values to -1 in the reformed array
+    1 = Current agent's game pieces
+    0 = No game pieces
+    -1 = Opposing agent's game pieces
 
     Params:
     c4_state - (6, 7, 2) array
-    agent_num - 1 or 2
 
     Returns:
     Tensor array of shape (6, 7)
     """
-        
-    main_agent_i = agent_num - 1
-    other_agent_i = 1 - main_agent_i
 
     result = np.zeros((6, 7), dtype=int)
-    result[c4_state[:, :, main_agent_i] == 1] = 1
-    result[c4_state[:, :, other_agent_i] == 1] = -1
+    result[c4_state[:, :, 0] == 1] = 1
+    result[c4_state[:, :, 1] == 1] = -1
 
     return torch.from_numpy(result.reshape(1, -1)).float()
   
   def get_Q_values(self, observation):
-    return self.dqn(self.convert_state_to_NN_input(observation['observation'], self.agent_num))
+    return self.dqn(self.convert_state_to_NN_input(observation['observation'] ))
--- main.py
+++ main.py
@@ -11,59 +11,54 @@ print(f"Using {device} device")
 
 def run(is_training=True):
   env = connect_four_v3.env(render_mode="human")
-  env.reset(seed=42)
 
-  agent0 = ConnectFourAgent(1)
-  agent1 = ConnectFourAgent(2)
+  agent0 = ConnectFourAgent()
+  agent1 = ConnectFourAgent()
 
-  if (is_training):
-    replay_memory = ReplayMemory(10000)  
+  for episode in range(5):
+    env.reset(seed=42)
 
-  last_observation = {env.agents[0]: None, env.agents[1]: None}
-  last_action = {env.agents[0]: None, env.agents[1]: None}
+    if (is_training):
+      replay_memory = ReplayMemory(10000)  
 
-  for agent in env.agent_iter():
-    # note: all these values are agent-specific
-    observation, reward, termination, truncation, info = env.last()
-    prev_observation = observation
+    last_observation = {env.agents[0]: None, env.agents[1]: None}
+    last_action = {env.agents[0]: None, env.agents[1]: None}
 
-    if termination or truncation:
-      action = None
-    else:
-      mask = observation["action_mask"]
+    for agent in env.agent_iter():
+      # note: all these values are agent-specific
+      observation, reward, termination, truncation, info = env.last()
 
-      if (agent == env.agents[0]):  
-        q_values = agent0.get_Q_values(observation)
+      if termination or truncation:
+        action = None
       else:
-        q_values = agent1.get_Q_values(observation)
+        if (agent == env.agents[0]):  
+          q_values = agent0.get_Q_values(observation)
+        else:
+          q_values = agent1.get_Q_values(observation)
 
-      # convert Tensor object to np array
-      if isinstance(q_values, torch.Tensor):
-        q_values = q_values.detach().cpu().numpy()
+        # convert Tensor object to np array
+        if isinstance(q_values, torch.Tensor):
+          q_values = q_values.detach().cpu().numpy()
 
-      # prevent doing any illegal actions (ex. certain columns are full)
-      if mask is not None:
-        q_values = np.where(mask, q_values, -np.inf)
+        action = int(np.argmax(q_values))
 
-      action = int(np.argmax(q_values))
+      env.step(action)
 
-    env.step(action)
+      # add entry to experience replay
+      if (is_training and not last_observation[agent] == None and len(env.agents) > 0):
+        cur_agent_num = 0 if agent == env.agents[0] else 1
+        opposing_agent_num = 1 - cur_agent_num
 
-    # add entry to experience replay
-    if (is_training and not last_observation[agent] == None and len(env.agents) > 0):
-      cur_agent_num = 0 if agent == env.agents[0] else 1
-      opposing_agent_num = 1 - cur_agent_num
+        # combines the state with that state's current player 
+        prev_state_input = np.append(last_observation[agent]["observation"], cur_agent_num)
+        cur_state_input = np.append(observation["observation"], opposing_agent_num)
 
-      # combines the state with that state's current player 
-      prev_state_input = np.append(last_observation[agent]["observation"], cur_agent_num)
-      cur_state_input = np.append(observation["observation"], opposing_agent_num)
+        replay_memory.append((prev_state_input, action, reward, cur_state_input, termination or truncation))
 
-      replay_memory.append((prev_state_input, action, reward, cur_state_input, termination or truncation))
-
-    # record the observation and action taken for the agent who just acted
-    last_observation[agent] = observation
-    last_action[agent] = action
-
+      # record the observation and action taken for the agent who just acted
+      last_observation[agent] = observation
+      last_action[agent] = action
 
   env.close()
+
 run()
