1d4c1a8c52c236a1d06d2f0c51b5a9c9398db001
Cameron Lee <cal26@sfu.ca>
2025-11-11 20:03:08 -0800

    add yaml file hyperparameter parsing

--- main.py
+++ main.py
@@ -5,60 +5,73 @@ import torch
 import numpy as np
 from agent import ConnectFourAgent
 from exp_replay import ReplayMemory
+import yaml
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
 
-def run(is_training=True):
-  env = connect_four_v3.env(render_mode="human")
+class GameEnvironment():
+  def __init__(self, hyperparameter_set):
+    with open('hyperparameters.yml', 'r') as f:
+      all_hyperparameter_sets = yaml.safe_load(f)
+      hyperparameters = all_hyperparameter_sets[hyperparameter_set]
+  
+    self.replay_memory_size = hyperparameters['replay_memory_size']
+    self.mini_batch_size = hyperparameters['mini_batch_size']
+    self.epsilon_init = hyperparameters['epsilon_init']
+    self.epsilon_decay = hyperparameters['epsilon_decay']
+    self.epsilon_min = hyperparameters['epsilon_min']
 
-  agent0 = ConnectFourAgent()
-  agent1 = ConnectFourAgent()
+  def run(self, is_training=True):
+    env = connect_four_v3.env(render_mode="human")
 
-  for episode in range(5):
-    env.reset(seed=42)
+    agent0 = ConnectFourAgent()
+    agent1 = ConnectFourAgent()
 
-    if (is_training):
-      replay_memory = ReplayMemory(10000)  
+    for episode in range(5):
+      env.reset(seed=42)
 
-    last_observation = {env.agents[0]: None, env.agents[1]: None}
-    last_action = {env.agents[0]: None, env.agents[1]: None}
+      if (is_training):
+        replay_memory = ReplayMemory(self.replay_memory_size)  
 
-    for agent in env.agent_iter():
-      # note: all these values are agent-specific
-      observation, reward, termination, truncation, info = env.last()
+      last_observation = {env.agents[0]: None, env.agents[1]: None}
+      last_action = {env.agents[0]: None, env.agents[1]: None}
 
-      if termination or truncation:
-        action = None
-      else:
-        if (agent == env.agents[0]):  
-          q_values = agent0.get_Q_values(observation)
+      for agent in env.agent_iter():
+        # note: all these values are agent-specific
+        observation, reward, termination, truncation, info = env.last()
+
+        if termination or truncation:
+          action = None
         else:
-          q_values = agent1.get_Q_values(observation)
+          if (agent == env.agents[0]):  
+            q_values = agent0.get_Q_values(observation)
+          else:
+            q_values = agent1.get_Q_values(observation)
 
-        # convert Tensor object to np array
-        if isinstance(q_values, torch.Tensor):
-          q_values = q_values.detach().cpu().numpy()
+          # convert Tensor object to np array
+          if isinstance(q_values, torch.Tensor):
+            q_values = q_values.detach().cpu().numpy()
 
-        action = int(np.argmax(q_values))
+          action = int(np.argmax(q_values))
 
-      env.step(action)
+        env.step(action)
 
-      # add entry to experience replay
-      if (is_training and not last_observation[agent] == None and len(env.agents) > 0):
-        cur_agent_num = 0 if agent == env.agents[0] else 1
-        opposing_agent_num = 1 - cur_agent_num
+        # add entry to experience replay
+        if (is_training and not last_observation[agent] == None and len(env.agents) > 0):
+          cur_agent_num = 0 if agent == env.agents[0] else 1
+          opposing_agent_num = 1 - cur_agent_num
 
-        # combines the state with that state's current player 
-        prev_state_input = np.append(last_observation[agent]["observation"], cur_agent_num)
-        cur_state_input = np.append(observation["observation"], opposing_agent_num)
+          # combines the state with that state's current player 
+          prev_state_input = np.append(last_observation[agent]["observation"], cur_agent_num)
+          cur_state_input = np.append(observation["observation"], opposing_agent_num)
 
-        replay_memory.append((prev_state_input, action, reward, cur_state_input, termination or truncation))
+          replay_memory.append((prev_state_input, action, reward, cur_state_input, termination or truncation))
 
-      # record the observation and action taken for the agent who just acted
-      last_observation[agent] = observation
-      last_action[agent] = action
+        # record the observation and action taken for the agent who just acted
+        last_observation[agent] = observation
+        last_action[agent] = action
 
-  env.close()
+    env.close()
 
-run()
+GameEnvironment("connect4").run()
