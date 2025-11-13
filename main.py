95beb7265b669c33dac15344137f5ef23cb437b5
Cameron Lee <cal26@sfu.ca>
2025-11-12 20:11:21 -0800

    implement greedy epsilon

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 from agent import ConnectFourAgent
 from exp_replay import ReplayMemory
 import yaml
+import random
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 print(f"Using {device} device")
@@ -21,6 +22,7 @@ class GameEnvironment():
     self.epsilon_init = hyperparameters['epsilon_init']
     self.epsilon_decay = hyperparameters['epsilon_decay']
     self.epsilon_min = hyperparameters['epsilon_min']
+    self.epsilon = self.epsilon_init
 
   def run(self, is_training=True):
     env = connect_four_v3.env(render_mode="human")
@@ -34,6 +36,10 @@ class GameEnvironment():
       if (is_training):
         replay_memory = ReplayMemory(self.replay_memory_size)  
 
+        # these variables are currently not used, could be useful for evaluating the model?
+        reward_history = []
+        epsilon_history = []
+
       last_observation = {env.agents[0]: None, env.agents[1]: None}
       last_action = {env.agents[0]: None, env.agents[1]: None}
 
@@ -44,16 +50,21 @@ class GameEnvironment():
         if termination or truncation:
           action = None
         else:
-          if (agent == env.agents[0]):  
-            q_values = agent0.get_Q_values(observation)
+          if (random.random() < self.epsilon):
+            # takes random action (exploration)
+            action = env.action_space(agent).sample()
           else:
-            q_values = agent1.get_Q_values(observation)
+            # takes action with max q value
+            if (agent == env.agents[0]):  
+              q_values = agent0.get_Q_values(observation)
+            else:
+              q_values = agent1.get_Q_values(observation)
 
-          # convert Tensor object to np array
-          if isinstance(q_values, torch.Tensor):
-            q_values = q_values.detach().cpu().numpy()
+            # convert Tensor object to np array
+            if isinstance(q_values, torch.Tensor):
+              q_values = q_values.detach().cpu().numpy()
 
-          action = int(np.argmax(q_values))
+            action = int(np.argmax(q_values))
 
         env.step(action)
 
@@ -66,11 +77,19 @@ class GameEnvironment():
           prev_state_input = np.append(last_observation[agent]["observation"], cur_agent_num)
           cur_state_input = np.append(observation["observation"], opposing_agent_num)
 
-          replay_memory.append((prev_state_input, action, reward, cur_state_input, termination or truncation))
+          prev_state_input_tensor = torch.tensor(prev_state_input, dtype=torch.int64, device=device)
+          action_tensor = torch.tensor(action, dtype=torch.int64, device=device) if not action == None else torch.tensor(-1, dtype=torch.int64, device=device)
+          reward_tensor = torch.tensor(reward, dtype=torch.int64, device=device)
+          cur_state_input_tensor = torch.tensor(cur_state_input, dtype=torch.int64, device=device)
+
+          replay_memory.append((prev_state_input_tensor, action_tensor, reward_tensor, cur_state_input_tensor, termination or truncation))
 
         # record the observation and action taken for the agent who just acted
         last_observation[agent] = observation
         last_action[agent] = action
+    
+    # decay epsilon
+    self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
 
     env.close()
 
